27509cf6bd1d6fa5e1a0ed3ff9540f1ccacd59cf
Nuno Bajanca <nunombajanca@gmail.com>
2015-05-25 03:17:45 +0100

    [Finished] Relauncher
    Only goes down if told so by the server.
    Fault tolerance and thread closing implemented.

--- Project/src/relauncher.c
+++ Project/src/relauncher.c
@@ -4,18 +4,25 @@
 #include <errno.h>
 #include <unistd.h>
 
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
 #include <pthread.h>
 
 #include "client-server.pb-c.h"
 #include "server-relauncher.h"
 #include "coms.h"
 
+#define BIND 0
+#define UNDEFINED 1
+
 int exit_server, parent;
 
 void * keep_parent_alive_thread(void *arg);
 void * keep_son_alive_thread(void *arg);
 void * send_alive_thread(void *arg);
-int createServer();
+int createServer(int error_type);
 
 pthread_t keep_parent_alive_thread_id;
 pthread_t keep_son_alive_thread_id;
@@ -32,11 +39,11 @@ void * keep_parent_alive_thread(void *arg){
 		sleep(2);
 		message_from_server = receiveMessageFIFO(fd_fifo);
 		if (message_from_server == NULL){
-			printf("Server not responding\n");
+			printf("[System Relauncher] Server not responding\n");
 			if (error == 2) should_exit = 1;
 			else error ++;
 		}else{
-			alive_message = alive__unpack(NULL, message_from_server->msg_size, message_from_server->msg);
+			alive_message = alive__unpack(NULL, message_from_server->msg_size, (const uint8_t *) message_from_server->msg);
 			if (alive_message == NULL){
 				if (error == 2) should_exit = 1;
 				else error ++;
@@ -46,10 +53,11 @@ void * keep_parent_alive_thread(void *arg){
 				}else if (alive_message->state == 1){
 					should_exit = 1;
 					exit_server = 1;
-					printf("Server turned off by admin. Closing Relauncher...\n");
+					printf("[System Relauncher] Server turned off by admin. Closing Relauncher...\n");
 				}
 			
 			}
+			alive__free_unpacked(alive_message, NULL);
 		}
 	}
 	
@@ -67,20 +75,21 @@ void * keep_son_alive_thread(void *arg){
 	int status;
 	int pid_proc;
 	
-	createServer();
+	createServer(UNDEFINED);
 	while ( !should_exit ){
 		pid_proc = wait(&status);
 		if(WIFEXITED(status) &&  !WIFSIGNALED(status)){
 			if (WEXITSTATUS(status) == 0){
 				should_exit = 1;
 				exit_server = 1;
-				printf("Server turned off by admin. Closing Relauncher...\n"); fflush(stdout);
+				printf("[System Relauncher] Server turned off by admin. Closing Relauncher...\n"); fflush(stdout);
 			}
 		}
 		
 		if (should_exit == 0){
-			printf("Server (%d) returned with %d code. Rebooting Server...\n", pid_proc, WEXITSTATUS(status));
-			createServer();
+			printf("[System Relauncher] Server (%d) returned with %d code. Rebooting Server...\n", pid_proc, WEXITSTATUS(status));
+			if(WEXITSTATUS(status) == 254) createServer(BIND);
+			else createServer(UNDEFINED);
 		}
 	}
 	
@@ -90,20 +99,20 @@ void * keep_son_alive_thread(void *arg){
 void * send_alive_thread(void *arg){
 	int fd_fifo = openFIFO(0);
 	int should_exit = 0;
-	proto_msg * message_to_server;
 	ALIVE alive_message;
 	alive__init(&alive_message);
 	alive_message.state = 0;
+	proto_msg * message_to_server = protoCreateAlive(&alive_message);
 	
 	while(! should_exit){
 		sleep(2);
-		message_to_server = protoCreateAlive(&alive_message);
 		if (write(fd_fifo, message_to_server->msg, message_to_server->msg_size) == -1){
-			perror("Write ");
+			perror("[System Relauncher Error] Write ");
 			should_exit = 1;
 		}
 	}
 	
+	destroyProtoMSG(message_to_server);
 	pthread_exit(NULL);
 }
 
@@ -111,34 +120,38 @@ int main ( int argc, char **argv ){
 	exit_server = 0;
 	
 	if (argc > 1){
-		printf("Relauncher started by the Server. Runing...\n");
 		parent = 0;
+		printf("[System Relauncher] Relauncher started by the Server. Runing...\n");
 		pthread_create(&keep_parent_alive_thread_id, NULL, keep_parent_alive_thread, NULL);
 	}else{
-		printf("Relauncher started. Starting Server...\n");
 		parent =1;
+		printf("[System Relauncher] Relauncher started. Starting Server...\n");
 		pthread_create(&send_alive_thread_id, NULL, send_alive_thread, NULL);
 		pthread_create(&keep_son_alive_thread_id, NULL, keep_son_alive_thread, NULL);
 	}
 	
-	
-	
 	while (1){
+		sleep(2);
 		if(exit_server == 1) break;
 	}
-	
+	pthread_join(keep_parent_alive_thread_id, NULL);
+	pthread_join(keep_son_alive_thread_id, NULL);
+	pthread_join(send_alive_thread_id, NULL);
 	exit(0);
 }
 
-int createServer(){
-	
+int createServer(int error_type){
 	int f_ret;
 	f_ret = fork();
 	char * v[1] = {NULL};
 	
 	//Fork
 	if (!f_ret){ // IF SON
-		printf("Server created by Relauncher\n");
+		printf("[System Relauncher] Server created by Relauncher\n");
+		if (error_type == BIND){
+			printf("[System Relauncher] Waiting 30 seconds to reboot (bind error on server)\n");
+			sleep(30);
+		}
 		return execve( "server", v , NULL); // Create Process
 	}else //PARENT
 		return f_ret;
